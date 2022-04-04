e41c341712da051901f050fa65f0380c33c0dc08
willschottler <wschottler@wschottler.com>
2022-04-04 18:51:22 -0400

    test: still trying to get the clock function scanned

--- react/index.js
+++ react/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Clock from './app';
+import Clock from '../src/clock';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Clock />, document.getElementById('root'));
--- react/stuff.js
+++ react/stuff.js
@@ -1,21 +1,3 @@
-function renderFullPage(html, preloadedState) {
-    return `
-    <!doctype html>
-    <html>
-        <head>
-            <title>Redux Universal Example</title>
-        </head>
-        <body>
-            <div id="root">${html}</div>
-            <script>
-                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
-            </script>
-            <script src="/static/bundle.js"></script>
-        </body>
-    </html>
-    `
-}
-
 express().get('/news', (req, res) => {
     let topic = req.query.topic;
     res.send(`<h1>${topic}</h1>`);
