169052db713fa57806fe0b84dd6d0ef23a166a6f
willschottler <wschottler@wschottler.com>
2022-04-04 15:56:44 -0400

    test: if this does not show a vulnerability I am going to question if this is scanning

--- react/app.js
+++ react/app.js
@@ -15,3 +15,8 @@ function renderFullPage(html, preloadedState) {
     </html>
     `
 }
+
+express().get('/news', (req, res) => {
+    let topic = req.query.topic;
+    res.send(`<h1>${topic}</h1>`);
+});
