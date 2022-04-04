19e640eea3c1e73e5a4ba9fb3184a89841d74e33
willschottler <wschottler@wschottler.com>
2022-04-04 18:21:49 -0400

    test: did an oops

--- react/app.js
+++ react/app.js
@@ -1,22 +1,16 @@
-function renderFullPage(html, preloadedState) {
-    return `
-    <!doctype html>
-    <html>
-        <head>
-            <title>Redux Universal Example</title>
-        </head>
-        <body>
-            <div id="root">${html}</div>
-            <script>
-                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
-            </script>
-            <script src="/static/bundle.js"></script>
-        </body>
-    </html>
-    `
-}
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
 
-express().get('/news', (req, res) => {
-    let topic = req.query.topic;
-    res.send(`<h1>${topic}</h1>`);
-});
+    render() {
+         // GOOD: this.state.date is defined above
+        var now = this.state.date.toLocaleTimeString()
+        return (
+                <div>
+                <h2>The time is {now}.</h2>
+                </div>
+        );
+    }
+}
